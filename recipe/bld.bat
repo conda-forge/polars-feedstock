d8c4010f2b62a72a92e1b459b7b30c28d08c2791
Maxyme <maxyme@outlook.com>
2022-06-09 10:35:43 -0400

    Enable Windows builds.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,3 +4,6 @@ if errorlevel 1 exit 1
 
 FOR /F "delims=" %%i IN ('dir /s /b target\wheels\*.whl') DO set polars_wheel=%%i
 %PYTHON% -m pip install --ignore-installed --no-deps %polars_wheel% -vv
+if errorlevel 1 exit 1
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
