3f5376d73c4148807afa036e5803f02ac8f22b21
Gert Hulselmans <hulselmansgert@gmail.com>
2022-08-19 14:03:44 +0200

    Fix Polars 0.13.58 build and newer. (#36)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 set "PYO3_PYTHON=%PYTHON%"
 
 set "CMAKE_GENERATOR=NMake Makefiles"
-maturin build --no-sdist --release --strip --manylinux off --interpreter=%PYTHON%
+maturin build --release --strip --manylinux off --interpreter=%PYTHON%
 if errorlevel 1 exit 1
 
 FOR /F "delims=" %%i IN ('dir /s /b target\wheels\*.whl') DO set polars_wheel=%%i
