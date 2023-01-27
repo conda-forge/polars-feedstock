50120c7c8e4b4de0bbbc479f792d1c0fea62be29
Kyle Husmann <kyle.husmann@gmail.com>
2023-01-27 13:28:00 -0500

    fix build on osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,12 @@ if [[ "${target_platform}" == "linux-64" ]]; then
   export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+sse4.2,+popcnt,+avx,+fma'
 fi
 
+echo "${target_platform}" # For debug, will remove
+
+if [[ "${target_platform}" == "osx-64" ]]; then
+  export SYSTEM_VERSION_COMPAT=0
+fi
+
 maturin build --release --strip --manylinux off --interpreter="${PYTHON}"
 
 "${PYTHON}" -m pip install $SRC_DIR/target/wheels/polars*.whl --no-deps -vv
