0fe0514716bdc3bd8f7dda333e050db7c74e5f26
Tim Paine <t.paine154@gmail.com>
2023-02-04 15:51:33 -0500

    try specifying specific interpreter

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,7 @@ if [[ "${target_platform}" == "linux-64" ]]; then
   export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+sse4.2,+popcnt,+avx,+fma'
 fi
 
-maturin build --release --strip --manylinux off --interpreter="${PYTHON}"
+maturin build --release --strip --manylinux off --interpreter "${PYTHON}"
 
 "${PYTHON}" -m pip install $SRC_DIR/target/wheels/polars*.whl --no-deps -vv
 
