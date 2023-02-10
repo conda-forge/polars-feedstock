94b9c6c9d38beea5050b68efe58e8961f4aa6bf5
Tim Paine <t.paine154@gmail.com>
2023-02-10 09:10:26 -0500

    try to add arm-specific Maturin args

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,12 +2,16 @@
 
 set -ex
 
+EXTRA_ARGS=
+
 if [[ "${target_platform}" == "linux-64" ]]; then
   # similar to settings upstream in polars
   export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+sse4.2,+popcnt,+avx,+fma'
+elif [[ "${target_platform}" == "osx-arm64" ]]; then
+ EXTRA_ARGS="--target aarch64-apple-darwin"  
 fi
 
-maturin build --release --strip --manylinux off --interpreter "${PYTHON}"
+maturin build --release --strip --manylinux off --interpreter "${PYTHON}" ${EXTRA_ARGS}
 
 "${PYTHON}" -m pip install $SRC_DIR/target/wheels/polars*.whl --no-deps -vv
 
