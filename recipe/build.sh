03e4e42470682edfd2f96b2ce8208d03e7be29a8
Oliver Borchert <borchero@icloud.com>
2023-05-18 23:30:44 +0200

    Update recipe/build.sh
    Co-authored-by: Uwe L. Korn <xhochy@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,6 @@ if [[ "${target_platform}" == "linux-64" ]]; then
   # similar to settings upstream in polars
   export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+sse4.2,+popcnt,+avx,+fma'
 elif [[ "${target_platform}" == "linux-aarch64" ]]; then
-  export RUSTFLAGS='-C codegen-units=4'
   sed -i "s/lto = ""fat""/lto = ""thin""/g" Cargo.toml
 fi
 
