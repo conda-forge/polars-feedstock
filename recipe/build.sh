9bb6942257bb96701dd2607b64afc8bbebbafb5f
Tanaka de Silva <tdesilva@proda.ai>
2023-06-10 21:51:48 +0100

    Use thin LTO

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,8 @@ set -ex
 if [[ "${target_platform}" == "linux-64" ]]; then
   # similar to settings upstream in polars
   export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+sse4.2,+popcnt,+avx,+fma'
+elif [[ "${target_platform}" == "linux-aarch64" ]]; then
+  sed -i "s/lto = ""fat""/lto = ""thin""/g" Cargo.toml
 fi
 
 # Run the maturin build via pip which works for direct and
