0173f3035ffe060c65fe428d7d0b836016360d67
Bela Stoyan <bela.stoyan@gmail.com>
2023-08-19 14:07:00 +0200

    use mold as linker

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,15 @@ if [[ "${target_platform}" == "linux-64" ]]; then
   export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+sse4.2,+popcnt,+avx,+fma'
 fi
 
+
+# Use mold as the linker
+mkdir -p .cargo
+cat <<EOF > .cargo/config
+[target.${rustc_target}]
+linker = "clang"
+rustflags = ["-C", "link-arg=-fuse-ld=${$PREFIX}/bin/mold"]
+EOF
+
 # Run the maturin build via pip which works for direct and
 # cross-compiled builds.
 $PYTHON -m pip install . -vv
