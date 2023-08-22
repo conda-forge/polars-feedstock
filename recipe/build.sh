de3decb651da7a4a8634c5c657b9ed6797a35747
Uwe L. Korn <uwe.korn@quantco.com>
2023-08-22 09:24:31 +0200

    Unvendor libgit2

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,8 @@ if [[ "${target_platform}" == "linux-64" ]]; then
   export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+sse4.2,+popcnt,+avx,+fma'
 fi
 
-echo rustc --version
+export LIBGIT2_NO_VENDOR=1
+export CARGO_BUILD_RUSTFLAGS="${CARGO_BUILD_RUSTFLAGS} -C link-arg=-Wl,-L$PREFIX/lib"
 
 # Run the maturin build via pip which works for direct and
 # cross-compiled builds.
