d81d9b49c8ad60fec180ed73fcc43bf1fd1e4327
Pavel Zwerschke <pavelzw@gmail.com>
2024-11-14 10:52:28 +0100

    debug

--- recipe/build.sh
+++ recipe/build.sh
@@ -45,6 +45,8 @@ rustc --version
 if [[ ("${target_platform}" == "win-64" && "${build_platform}" == "linux-64") ]]; then
   ls -la /opt
 
+  cat ~/conda_build_config.yaml
+
   ls -la /opt/winsdk-10.0.22621.0
 
   cat /opt/winsdk-10.0.22621.0/winsdk_vfs_overlay.yaml
