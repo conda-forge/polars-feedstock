6092a7fe880022b53a8a1ec7f7166ee8a647c7f6
Uwe L. Korn <uwe.korn@quantco.com>
2024-11-21 10:47:16 +0100

    Allow for osx cross compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -44,7 +44,7 @@ fi
 
 rustc --version
 
-if [[ ("${target_platform}" == "win-64" && "${build_platform}" == "linux-64") ]]; then
+if [[ ("${target_platform}" == "win-64" && "${build_platform}" != "win-64") ]]; then
   # we need to add the generate-import-lib feature since otherwise
   # maturin will expect libpython DSOs at PYO3_CROSS_LIB_DIR
   # which we don't have since we are not able to add python as a host dependency
