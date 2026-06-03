57529070ae257ff1aec7232d2f7c4e4ae251f758
Michał Górny <mgorny@quansight.com>
2026-06-03 12:07:07 +0200

    MNT: Re-rendered with conda-smithy 3.61.3.dev1+g7d03432e1 and conda-forge-pinning 2026.06.03.07.30.59
    Other tools:
    - conda-build 26.5.0
    - rattler-build 0.65.0
    - rattler-build-conda-compat 1.4.14

--- .scripts/free_disk_space.bat
+++ .scripts/free_disk_space.bat
@@ -4,7 +4,7 @@ set CLEANUP_DIRS=^
 C:\hostedtoolcache\windows;^
 ;
 
-del /f/s/q %CLEANUP_DIRS%
+del /f/s/q %CLEANUP_DIRS% > nul
 rmdir /s/q %CLEANUP_DIRS%
 
 wmic logicaldisk get size,freespace,caption
