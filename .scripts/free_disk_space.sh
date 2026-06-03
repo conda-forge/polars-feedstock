5bdb5d143ee9dad0bf312be172b033480e839b58
Michał Górny <mgorny@quansight.com>
2026-06-03 11:44:41 +0200

    MNT: Re-rendered with conda-smithy 3.61.3.dev1+g7d03432e1 and conda-forge-pinning 2026.06.03.07.30.59
    Other tools:
    - conda-build 26.5.0
    - rattler-build 0.65.0
    - rattler-build-conda-compat 1.4.14

--- .scripts/free_disk_space.sh
+++ .scripts/free_disk_space.sh
@@ -5,15 +5,18 @@ set -ex
 FREE_DISK_SPACE=${1}
 
 if [[ ${FREE_DISK_SPACE} == *,cache,* ]]; then
-  sudo find \
-           /opt/ghc \
-           /opt/hostedtoolcache \
-           /usr/lib/jvm \
-           /usr/local/.ghcup \
-           /usr/local/lib/android \
-           /usr/local/share/powershell \
-           /usr/share/dotnet \
-           /usr/share/swift -delete
+  DIRS_TO_REMOVE=(
+    /opt/ghc
+    /opt/hostedtoolcache
+    /usr/lib/jvm
+    /usr/local/.ghcup
+    /usr/local/lib/android
+    /usr/local/share/powershell
+    /usr/share/dotnet
+    /usr/share/swift
+  )
+
+  sudo rm -rf "${DIRS_TO_REMOVE[@]}"
 fi
 
 if [[ ${FREE_DISK_SPACE} == *,apt,* ]]; then
