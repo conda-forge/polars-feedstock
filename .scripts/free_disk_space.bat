580546b53f458089f7006285687116404a2fdfd1
Michał Górny <mgorny@quansight.com>
2026-06-03 12:24:20 +0200

    MNT: Re-rendered with conda-smithy 3.61.3.dev1+g7d03432e1 and conda-forge-pinning 2026.06.03.07.30.59
    Other tools:
    - conda-build 26.5.0
    - rattler-build 0.65.0
    - rattler-build-conda-compat 1.4.14

--- .scripts/free_disk_space.bat
+++ .scripts/free_disk_space.bat
@@ -1,19 +1,6 @@
 setlocal enableextensions enabledelayedexpansion
 
-set CLEANUP_DIRS=^
-C:\hostedtoolcache\windows;^
-;
-
-mkdir C:\empty
-for %%f in (%CLEANUP_DIRS:;= %) do (
-    if not [%%f] == [] (
-        echo Removing %%f
-        dir %%f
-        robocopy /mir /mt /zb /ns /nc /nfl /ndl /np /njh /njs C:\empty %%f > nul 2>&1
-        rmdir /q %%f
-    )
-)
-rmdir /q C:\empty
+powershell -Command "Remove-Item -LiteralPath 'C:\hostedtoolcache\windows' -Force -Recurse"
 
 wmic logicaldisk get size,freespace,caption
 exit /b
