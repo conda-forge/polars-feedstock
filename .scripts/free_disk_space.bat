4f1ba6ec4c542c9e08ac073d2be2faa012dfc5b8
Michał Górny <mgorny@quansight.com>
2026-06-05 14:59:23 +0200

    stale file
    Signed-off-by: Michał Górny <mgorny@quansight.com>

--- .scripts/free_disk_space.bat
+++
@@ -1,6 +0,0 @@
-setlocal enableextensions enabledelayedexpansion
-
-powershell -Command "Remove-Item -LiteralPath 'C:\hostedtoolcache\windows' -Force -Recurse"
-
-wmic logicaldisk get size,freespace,caption
-exit /b
